a4332dee21c0c71cc730e00faed26da5ad8a919f
bilinc PC <bilinc@kth.se>
2021-02-06 23:22:35 +0100

    Sucess for regularized lin reg cost function and gradient calculation

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -18,11 +18,13 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+h = X*theta;
+J = 1/(2*m)*sum((h-y).^2) + lambda/(2*m)*sum(theta(2:end).^2);
 
 
+grad(1) += 1/m*sum((h-y)*X(1));
 
-
-
+grad(2:end) += 1/m*sum((h-y).*X(:, 2:end))' + lambda*theta(2:end)/m;
 
 
 
